e1082cb503ac2da45f2420af2fa64cbbef148d79
Romina Aguado <rominaaguado@Rominas-MacBook-Pro.local>
2020-11-08 18:46:16 -0600

    added Jquery to display date at top

---
+++ scheduler.js
@@ -0,0 +1,62 @@
+//GIVEN I am using a daily planner to create a schedule
+
+
+
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+
+
+
+//////// to Display date in jumbotron /////
+console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+
+///to save entries in local storage//
+
+//localStorage.setItem("savebtn")
+ $(".saveBtn").on("click",function() {
+    
+        let dayActivity = $(this).siblings().val();  
+        console.log(dayActivity);
+
+    let hour = $(this).parent().attr("id");
+    console.log(hour);   
+    
+    localStorage.setItem(hour, dayActivity);
+
+ });
+
+ $("#hour-9 .time-block").val(localStorage.getItem("hour-9"));
+ $("#hour-10 .time-block").val(localStorage.getItem("hour-10"));
+ $("#hour-11 .time-block").val(localStorage.getItem("hour-11"));
+ $("#hour-12 .time-block").val(localStorage.getItem("hour-12"));
+ $("#hour-13 .time-block").val(localStorage.getItem("hour-13"));
+ $("#hour-14 .time-block").val(localStorage.getItem("hour-14"));
+ $("#hour-15 .time-block").val(localStorage.getItem("hour-15"));
+ $("#hour-16 .time-block").val(localStorage.getItem("hour-16"));
+ $("#hour-17 .time-block").val(localStorage.getItem("hour-17"));
+
+
+
+
+
+
+////to change colors when it's past, present, future///
+
+// var themeSwitcher = document.querySelector("#theme-switcher");
+// var container = document.querySelector(".container");
+// var mode = "dark";
+
+// themeSwitcher.addEventListener("click", function() {
+//   if (mode === "dark") {
+//     mode = "light";
+//     container.setAttribute("class", "light");
+//   }
+//   else {
+//     mode = "dark";
+//     container.setAttribute("class", "dark");
+//   }
+// // });
+
